1b01d8ebc36fcca51acae9409a22005a8b5149a6
jaduplansti <84189727+jaduplansti@users.noreply.github.com>
2023-04-27 12:54:04 +0800

    Update node.c

--- src/node.c
+++ src/node.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "node.h"
 
 root_node *new_node() {
   return calloc(1, sizeof(root_node));
 }
 
 conditional_node *new_con_node(binary_expr *expr) {
-  
+  conditional_node *con_node = calloc(1, sizeof(conditional_node));
+  con_node->condition = expr;
+
+  return con_node;
 }
