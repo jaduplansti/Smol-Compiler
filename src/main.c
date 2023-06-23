49ba1facbb278c5439d0cac672e3d313fd060f63
jaduplansti <84189727+jaduplansti@users.noreply.github.com>
2023-06-23 17:21:11 +0800

    Fixed main

--- src/main.c
+++ src/main.c
@@ -7,20 +7,46 @@ FILE *src;
 int main(int argc, char **argv) {
 
   if(argc < 2) {
-    fprintf(stderr,"No Source File Specified\n");
-    return 1;
+    fprintf(stderr,"No Arguement Specified\n");
+    return -1;
   }
 
-  for(size_t argc_i = 1 argc_i < argc; argc_i++) { /* Loops Over Argc */
-    for(size_t argc_c = 0; argc_c < strlen(argc[argc_i]); argc_c++) { /* Loops Over Characters In Argc */
-      if(argc[argc_i][argc_c] == '-') { /* If Argc Character Is '-' */
-        switch(argc[argc_i][++argc_c]) {
+  FILE *src = NULL;
+  FILE *dst = NULL;
+
+  for(size_t argc_i = 1; argc_i < argc; argc_i++) { /* Loops Over Argc */
+    for(size_t argv_c = 0; argv_c < strlen(argv[argc_i]); argv_c++) { /* Loops Over Characters In Argv */
+      if(argv[argc_i][argv_c] == '-') { /* If Argc Character Is '-' */
+        switch(argv[argc_i][++argv_c]) {
           case 's':
-            /* Insert Code Here */
+            if(argv[++argc_i] == NULL) {
+              fprintf(stderr, "You Forgot To Include A Source File After \'-s\' \n");
+              return -1;
+            }
+
+            src = fopen(argv[argc_i], "r");
+            
+            if(src == NULL) {
+                fprintf(stderr, "File \"%s\" Does Not Exist\n", argv[argc_i]);
+                return -1;
+            }
+            
           break;
 
           case 'o':
-            /* Insert Code Here */
+          
+             if(argv[++argc_i] == NULL) {
+              fprintf(stderr, "You Forgot To Include A Output File After \'-o\' \n");
+              return -1;
+            }
+
+            dst = fopen(argv[argc_i], "w+");
+            
+            if(dst == NULL) {
+                fprintf(stderr, "File \"%s\" Cannot Be Created\n", argv[argc_i]);
+                return -1;
+            }
+            
           break;
 
           default:
